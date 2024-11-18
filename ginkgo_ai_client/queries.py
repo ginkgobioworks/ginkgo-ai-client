fcca52a47b269ee529eabc8681ef782dafad7c89
Valentin Zulkower <vzulkower@ginkgobioworks.com>
2024-11-18 12:14:02 -0500

    add promoter activity inference, with tests

--- ginkgo_ai_client/queries.py
+++ ginkgo_ai_client/queries.py
@@ -2,6 +2,7 @@
 
 from typing import Dict, Optional, Any, List
 from abc import ABC, abstractmethod
+import json
 
 import pydantic
 
@@ -32,6 +33,13 @@ class QueryBase(pydantic.BaseModel, ABC):
     @abstractmethod
     def parse_response(self, results: Dict) -> Any:
         pass
+
+
+class ResponseBase(pydantic.BaseModel):
+
+    def write_to_jsonl(self, path: str):
+        with open(path, "a") as f:
+            f.write(self.model_dump_json() + "\n")
 
 
 _maskedlm_models_properties = {
@@ -67,7 +75,7 @@ def _validate_model_and_sequence(model, sequence: str, allow_masks=False):
         raise ValueError("Invalid sequence type")
 
 
-class EmbeddingResponse(pydantic.BaseModel):
+class EmbeddingResponse(ResponseBase):
     """A response to a MeanEmbeddingQuery, with attributes `embedding` (the mean
     embedding of the model's last encoder layer) and `query_name` (the original
     query's name).
@@ -143,7 +151,7 @@ class MeanEmbeddingQuery(QueryBase):
         return list(cls.iter_from_fasta(fasta_path, model))
 
 
-class SequenceResponse(pydantic.BaseModel):
+class SequenceResponse(ResponseBase):
     """A response to a MaskedInferenceQuery, with attributes `sequence` (the predicted
     sequence) and `query_name` (the original query's name).
     """
@@ -211,3 +219,159 @@ auto_doc_str = f"""
 
 for cls in [MeanEmbeddingQuery, MaskedInferenceQuery]:
     cls.__doc__ += auto_doc_str[:1]
+
+
+### PROMOTER ACTIVITY QUERIES ###
+
+
+class PromoterActivityResponse(ResponseBase):
+    """A response to a PromoterActivityQuery, with attributes `activity` (the predicted
+    activity) and `query_name` (the original query's name).
+
+    Attributes
+    ----------
+    activity_by_tissue: Dict[str, float]
+        The activity of the promoter in each tissue.
+    query_name: Optional[str] = None
+        The name of the query. It will appear in the API response and can be used to
+        handle exceptions.
+    """
+
+    activity_by_tissue: Dict[str, float]
+    query_name: Optional[str] = None
+
+
+class PromoterActivityQuery(QueryBase):
+    """A query to infer the activity of a promoter in different tissues.
+
+    Parameters
+    ----------
+    promoter_sequence: str
+        The promoter sequence. Only ATGCN characters are allowed.
+    orf_sequence: str
+        The ORF sequence. Only ATGCN characters are allowed.
+    tissue_of_interest: Dict[str, List[str]]
+        The tissues of interest, with the tracks representing each tissue, for instance
+        `{"heart": ["CNhs10608+", "CNhs10612+"], "liver": ["CNhs10608+", "CNhs10612+"]}`.
+    query_name: Optional[str] = None
+        The name of the query. It will appear in the API response and can be used to
+        handle exceptions.
+    model: str = "borzoi-human-fold0"
+        The model to use for the inference (only one default model is supported for now).
+
+    Returns
+    -------
+    PromoterActivityResponse
+        ``client.send_request(query)`` returns a ``PromoterActivityResponse`` with
+        attributes ``activity_by_tissue`` (the activity of the promoter in each tissue)
+        and ``query_name`` (the original query's name).
+    """
+
+    promoter_sequence: str
+    orf_sequence: str
+    tissue_of_interest: Dict[str, List[str]]
+    model: str = "borzoi-human-fold0"
+    query_name: Optional[str] = None
+
+    def to_request_params(self) -> Dict:
+        # TODO: update the web API so the conversion isn't necessary
+        data = {
+            "prom": self.promoter_sequence,
+            "orf": self.orf_sequence,
+            "tissue_of_interest": self.tissue_of_interest,
+        }
+        return {
+            "model": self.model,
+            "text": json.dumps(data),
+            "transforms": [{"type": "PROMOTER_ACTIVITY"}],
+        }
+
+    def parse_response(self, results):
+        return PromoterActivityResponse(
+            query_name=self.query_name, activity_by_tissue=results
+        )
+
+    @pydantic.model_validator(mode="after")
+    def sequences_are_valid_nucleotide_sequences(cls, query):
+        """Raise an error if the sequences contain non-ATGCN characters."""
+        query.promoter_sequence = query.promoter_sequence.upper()
+        query.orf_sequence = query.orf_sequence.upper()
+        if not set(query.promoter_sequence).issubset(set("ATGCN")):
+            raise ValueError(
+                f"Promoter sequence in query <{query.query_name}> contains "
+                "non-ATGCN characters."
+            )
+        if not set(query.orf_sequence).issubset(set("ATGCN")):
+            raise ValueError(
+                f"ORF sequence in query <{query.query_name}> contains "
+                "non-ATGCN characters."
+            )
+        return query
+
+    @classmethod
+    def iter_with_promoter_from_fasta(
+        cls,
+        fasta_path: str,
+        orf_sequence: str,
+        tissue_of_interest: Dict[str, List[str]],
+        model: str = "borzoi-human-fold0",
+    ):
+        """Return an iterator of PromoterActivityQuery objects from the promoter
+        sequences in a fasta file. The iterator has a length attribute that gives the
+        number of sequences in the fasta file.
+
+        Parameters
+        ----------
+        fasta_path: str
+            The path to the fasta file containing the promoter sequences.
+        orf_sequence: str
+            The ORF sequence.
+        tissue_of_interest: Dict[str, List[str]]
+            The tissues of interest, with the tracks representing each tissue, e.g.
+            `{"heart": ["CNhs10608+", "CNhs10612+"], "liver": ["CNhs10608+", "CNhs10612+"]}`.
+        model: str = "borzoi-human-fold0"
+            The model to use for the inference (only one default model is supported for now).
+        """
+        fasta_iterator = fasta_sequence_iterator(fasta_path)
+        query_iterator = (
+            cls(
+                promoter_sequence=str(record.seq),
+                orf_sequence=orf_sequence,
+                tissue_of_interest=tissue_of_interest,
+                model=model,
+                query_name=record.id,
+            )
+            for record in fasta_iterator
+        )
+        return IteratorWithLength(query_iterator, len(fasta_iterator))
+
+    @classmethod
+    def list_with_promoter_from_fasta(
+        cls,
+        fasta_path: str,
+        orf_sequence: str,
+        tissue_of_interest: Dict[str, List[str]],
+        model: str = "borzoi-human-fold0",
+    ):
+        """Return a list of PromoterActivityQuery objects from the promoter sequences
+        in a fasta file.
+
+        Parameters
+        ----------
+        fasta_path: str
+            The path to the fasta file containing the promoter sequences.
+        orf_sequence: str
+            The ORF sequence.
+        tissue_of_interest: Dict[str, List[str]]
+            The tissues of interest, with the tracks representing each tissue, e.g.
+            `{"heart": ["CNhs10608+", "CNhs10612+"], "liver": ["CNhs10608+", "CNhs10612+"]}`.
+        model: str = "borzoi-human-fold0"
+            The model to use for the inference (only one default model is supported for now).
+        """
+        iterator = cls.iter_with_promoter_from_fasta(
+            fasta_path=fasta_path,
+            orf_sequence=orf_sequence,
+            tissue_of_interest=tissue_of_interest,
+            model=model,
+        )
+        return list(iterator)
