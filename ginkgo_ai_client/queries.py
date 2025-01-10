81bc0eee1630c3e6a7d492140bd50281c2e8569e
Valentin Zulkower <vzulkower@ginkgobioworks.com>
2025-01-10 15:40:01 -0500

    tweaks to mrna_foundation diffusion parameters

--- ginkgo_ai_client/queries.py
+++ ginkgo_ai_client/queries.py
@@ -502,13 +502,13 @@ class RNADiffusionMaskedQuery(QueryBase):
             "species": self.species,
             "temperature": self.temperature,
             "decoding_order_strategy": self.decoding_order_strategy,
-            "num_to_decode_per_step": self.unmaskings_per_step,
+            "unmaskings_per_step": self.unmaskings_per_step,
             "num_samples": self.num_samples,
         }
         return {
             "model": self.model,
             "text": json.dumps(data),
-            "transforms": [{"type": "GENERATE"}],
+            "transforms": [{"type": "MRNA_DIFFUSION_GENERATE"}],
         }
 
     def parse_response(self, results: Dict) -> MultimodalDiffusionMaskedResponse:
